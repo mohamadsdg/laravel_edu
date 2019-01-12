a0c5e294ccecbeaece54b14316ccecb5c72f2ea0
mohamad_sdg <sadeghi_mohamadreza@outlook.com>
2019-01-12 10:41:09 +0330

    fixed use and quick and dirty way

--- app/Http/Controllers/cardsController.php
+++ app/Http/Controllers/cardsController.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\DB;
+use DB;
 
 
 class cardsController extends Controller
 {
     public function index()
     {
-        $cards = DB::table('cards')->get();
+        $cards = DB::table('cards')->get(); /* use \DB(called quick and dirty way) instead of use DB in top of page*/
         return view('cards.index',compact('cards'));
     }
 }
