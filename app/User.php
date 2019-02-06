6b0304a417f9dd2cfc51e3b86eb5cf73c7c84b57
mohamad_sdg <sadeghi_mohamadreza@outlook.com>
2019-02-06 12:10:20 +0330

    authenticate: work whit auth

--- app/User.php
+++ app/User.php
@@ -2,12 +2,40 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+use Illuminate\Support\Facades\Auth;
+
+class User extends Authenticatable
 {
-    public function notes()
-    {
-        return $this->hasMany(Note::class);
-    }
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-/*Route::get('about',function (){
-    $people = ['mohamad','sina','ali'];
-//    return view('about', ['people' => $people]);
-//    return view('about', compact('people')); // compact return array and 1st parameter get new key same variable without $
-//    return view('about')->with('people',$people); // set key(param1) and variable(param2) for passing to view (we said chaining)
-//    return view('about')->withPeople($people); // set key(withPeople) and variable(param) for passing to view (we said chaining noe 2)
+Auth::routes();
 
-    return View::make('about', compact('people')); // used this syntax called facad View::make
-});*/
-
-
-Route::group(['middleware' => ['web']], function () {
-
-    Route::get('cont', 'pagesController@home');
-    Route::get('about', 'pagesController@about');
-    Route::get('card', 'cardsController@index');
-    Route::get('card/{card}', 'cardsController@show'); // {card} name is wildcard
-    Route::post('card/{card}/notes', 'NotesController@store'); // save note
-    Route::get('note/{note}/edit', 'NotesController@edit'); // show for edit note
-    Route::patch('notes/{note}', 'NotesController@update'); // update note
-
-});
+Route::get('/home',  'HomeController@index')->name('home');
